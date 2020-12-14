48482066fa05746d40351b6450e626430d04b872
Mikita Stryzhak <nikitastrizhak@mail.ru>
2020-12-14 16:35:11 +0300

    cpid test

--- libs/cpuid.cpp
+++ libs/cpuid.cpp
@@ -1,18 +1,22 @@
 #include "cpuid.h"
 
-#define REGISTER_CALL_DEPTH 6
+#include <iostream>
+
+#define REGISTER_CALL_DEPTH 0x18
 
 std::vector<uint32_t>
 hardware_identification::GetCPUId() {
     std::vector<uint32_t> id_key;
 
-    for (int i = 0; i < REGISTER_CALL_DEPTH;
+    for (int i = 5; i < REGISTER_CALL_DEPTH;
          i++) {
+
+        std::cout << i <<")" << std::endl;
         hardware_identification::CPUID cpu_id(i);
 
-        id_key.push_back(cpu_id.EBX());
         id_key.push_back(cpu_id.EDX());
         id_key.push_back(cpu_id.ECX());
+        std::cout <<cpu_id.EDX() << " " << cpu_id.ECX() << " " << std::endl << std::endl;
     }
     return id_key;
 }
